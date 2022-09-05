f6c4b4da49dc24021c06d8f4b9d719b04403d4a1
Marian Kolobych <marian.kolobych@euristiq.com>
2022-09-05 08:27:01 +0300

    Fixed severe production problem

--- src/main/java/net/proselyte/gitoverview/Feature12.java
+++ src/main/java/net/proselyte/gitoverview/Feature12.java
@@ -1,4 +1,9 @@
 package net.proselyte.gitoverview;
 
 public class Feature12 {
+    public String toString() {
+        return "BaseEntity" +
+                "id=" + id +
+                '}'
+    }
 }
